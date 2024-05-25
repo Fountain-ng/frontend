e44e2793771680139f0fb107b513d170cbca6a13
Pavilion-devs <olaboyefavour52@gmail.com>
2024-05-25 02:40:29 +0100

    Resolve merge conflicts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: '/my-fmcg-landing-page',
+    assetPrefix: '/my-fmcg-landing-page',
+    images: {
+      loader: 'default',
+      path: '/my-fmcg-landing-page/',
+    },
+  };
+  
+  export default nextConfig;
+  
+  
