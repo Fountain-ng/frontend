6bb7a79f3060d074a2028904ba7c5798e6151c9a
Pavilion-devs <olaboyefavour52@gmail.com>
2024-05-25 03:05:31 +0100

    Resolve merge conflicts

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/my-fmcg-landing-page',
-    assetPrefix: '/my-fmcg-landing-page',
     images: {
-      loader: 'default',
-      path: '/my-fmcg-landing-page/',
-    },
-  };
-  
-  export default nextConfig;
-  
-  
+        unoptimized: true,
+      },
+};
+
+export default nextConfig;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -18,7 +18,7 @@ const Hero = () => {
         <h1 className="hero__title">Refreshing Moments Await with Fountain Beverages</h1>
         
         <a href="#services" className="hero__cta">
-        <Image
+        <img
           src="/bx-down-arrow-alt.svg"
           className="hero__cta-img"
           alt="down arrow"
