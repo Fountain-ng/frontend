3342b478511bf6116867ce18467927aac07a4440
Pavilion-devs <olaboyefavour52@gmail.com>
2024-06-14 23:16:24 +0100

    Done

--- src/api/config/host.js
+++ src/api/config/host.js
@@ -1,9 +1,7 @@
-  // change this to use dev or prod host
-  export const currentHost = host.dev;
-=======
+export const host = Object.freeze({
   dev: "http://localhost:8080/api/v1",
   prod: "https://backend.fountain.ng/api/v1",
 });
 
 // change this to use dev or prod host
-export const current = host.dev;
+export const currentHost = host.dev;
