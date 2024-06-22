ffc6f63c450bf69d7eaf61e22bea6dbaf156ba78
coded1guy <grggmmnl@gmail.com>
2024-06-22 02:56:03 +0100

    refactor: arranged the api and added updated the routes

--- src/api/config/routes.js
+++ src/api/config/routes.js
@@ -6,32 +6,26 @@ export const routes = Object.freeze({
     status: `${currentHost}/auth/status`,
     refreshToken: `${currentHost}/auth/token/refresh`,
     logout: `${currentHost}/auth/logout`,
-    user: {
-      signup: `${currentHost}/auth/user/login`,
-      login: `${currentHost}/auth/user/signup`,
-      get: `${currentHost}/auth/user/get`,
-      updateBasic: `${currentHost}/auth/user/update/basic`,
-      updatePassword: `${currentHost}/auth/user/update/password`,
-    },
-    admin: {
-      signup: `${currentHost}/auth/admin/login`,
-      login: `${currentHost}/auth/admin/signup`,
-      get: `${currentHost}/auth/admin/get`,
-      updateBasic: `${currentHost}/auth/admin/update/basic`,
-      updatePassword: `${currentHost}/auth/admin/update/password`,
-    },
-    brand: {
-      create: `${currentHost}/brand/create`,
-      get: `${currentHost}/brand`,
-      update: `${currentHost}/brand/update`,
-    },
-    product: {
-      create: `${currentHost}/product/create/`,
-      get: `${currentHost}/product`,
-      getByBrand: `${currentHost}/product/brand/`,
-      update: `${currentHost}/product/update/`,
-      deleteOne: `${currentHost}/product/delete/`,
-      deleteByBrand: `${currentHost}/product/brand/`,
-    },
+    login: `${currentHost}/auth/user/login`,
+    signup: `${currentHost}/auth/user/signup`,
+  },
+  user: {
+    get: `${currentHost}/auth/user/get`,
+    updateBasic: `${currentHost}/auth/user/update/basic`,
+    updatePassword: `${currentHost}/auth/user/update/password`,
+  },
+  brand: {
+    get: `${currentHost}/brand`,
+  },
+  product: {
+    get: `${currentHost}/product`,
+    getByBrand: `${currentHost}/product/brand/`,
+  },
+  order: {
+    create: `${currentHost}/protected/order/create`,
+    get: `${currentHost}/protected/order/`,
+    getByUser: `${currentHost}/protected/order/user`,
+    update: `${currentHost}/protected/order/update/`,
+    deleteOne: `${currentHost}/protected/order/delete/`,
   },
 });
